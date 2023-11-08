e170b9a32cbca762908a4c31de383da623942fad
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-11-08 12:52:37 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :charges
   resources :users
+   post "/register", to: "users#create"
+  post "/login", to: "sessions#create"
+   delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
